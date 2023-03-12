b1342820b88d7f80a0b32a5e70efa7fbad77883e
pizzaYami <cyj9407@naver.com>
2023-03-12 12:20:22 +0900

    리액트 라우터1 : 셋팅이랑 기본 라우팅

--- shop/src/App.js
+++ shop/src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
@@ -6,11 +5,43 @@ import Navbar from 'react-bootstrap/Navbar';
 import bg from './img/bg.png';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import data from './data'
+import { useState } from 'react';
+import { Route, Routes, Link } from 'react-router-dom'
+import Detail from './detail';
 
 function App() {
+
+  let [shoes] = useState(data)
+
+
+  function Card(props) {
+    return (
+      <>
+        {
+          props.shoes.map(function (el, i) {
+            let src = `https://codingapple1.github.io/shop/shoes${i + 1}.jpg`
+
+            return (
+              <Col>
+                <div key={i}>
+                  <img src={src} width="80%" />
+                  <h4>{el.title}</h4>
+                  <p>{el.content}</p>
+                </div>
+              </Col>
+            )
+          })}
+      </>
+    )
+  }
+
+
+
   return (
-    <div>
-    <Navbar bg="primary" variant="dark">
+    <>
+
+      <Navbar bg="primary" variant="dark">
         <Container>
           <Navbar.Brand href="#home">조샵</Navbar.Brand>
           <Nav className="me-auto">
@@ -20,33 +51,30 @@ function App() {
           </Nav>
         </Container>
       </Navbar>
-      
-      <div className='main-bg' style={{backgroundImage:'url('+ bg +')'}}></div>
 
-      <Container>
-      <Row>
-        <Col>
-        <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="80%" />
-        <h4>상품명</h4>
-        <p>상품설명</p>
-        </Col>
-        <Col>
-        <img src="https://codingapple1.github.io/shop/shoes2.jpg" width="80%" />
-        <h4>상품명</h4>
-        <p>상품설명</p>
-        </Col>
-        <Col>
-        <img src="https://codingapple1.github.io/shop/shoes3.jpg" width="80%" />
-        <h4>상품명</h4>
-        <p>상품설명</p>
-        </Col>
-
-      </Row>
-    </Container>
+      <Link to="/">홈</Link>
+      <br />
+      <Link to="/detail">상세페이지</Link>
 
 
-      </div>
+      <Routes>
+        <Route path="/" element={
+          <Container>
+            <div className='main-bg' style={{ backgroundImage: 'url(' + bg + ')' }}>
+            </div>
+            <Row>
+              <Card shoes={shoes} />
+            </Row>
+          </Container>} />
+        <Route path="/detail" element={
+          <Detail />
+        } />
+      </Routes>
+    </>
   );
 }
+
+
+
 
 export default App;
