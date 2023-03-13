1a44dbb0255ca394f774a2487767848ac683cc38
pizzaYami <cyj9407@naver.com>
2023-03-13 11:22:09 +0900

    리액트 라우터 3 : URL 파라미터로 상세페이지 100개 만들기

--- shop/src/App.js
+++ shop/src/App.js
@@ -62,12 +62,7 @@ function App() {
               <Card shoes={shoes} />
             </Row>
           </Container>} />
-        <Route path="/detail" element={<Detail />} />
-
-        <Route path="/event" element={<Event></Event>}>
-          <Route path="one" element={<div> 첫 주문시 양배추즙 서비스</div>} />
-          <Route path="two" element={<div>생일기념 쿠폰받기</div>} />
-        </Route>
+        <Route path="/detail/:id" element={<Detail shoes={shoes}/>} />
       </Routes>
     </>
   );
