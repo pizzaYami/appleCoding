686b8e549eeeb4d96c437836b93edb7f80268665
조용주 <joyongju@joyongjuui-MacBookAir.local>
2023-05-07 18:54:48 +0900

    폼에 입력한 데이터를 서버에 전송하는 법 (POST요청)

--- todoapp/server.js
+++ todoapp/server.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.listen(8080, function () {
   console.log("listening on 8080");
@@ -13,4 +15,9 @@ app.get("/", function (req, res) {
 });
 app.get("/write", function (req, res) {
   res.sendFile(__dirname + "/write.html");
+});
+app.post("/add", function (req, res) {
+  console.log(req.body.title);
+  console.log(req.body.data);
+  res.send("전송 완료");
 });
