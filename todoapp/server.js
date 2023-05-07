1b7549f882a2f614779f7aa45fa5d8ae39819e76
조용주 <joyongju@joyongjuui-MacBookAir.local>
2023-05-07 17:53:04 +0900

    bootstrap 숙제

--- todoapp/server.js
+++ todoapp/server.js
@@ -11,3 +11,6 @@ app.get("/pet", function (req, res) {
 app.get("/", function (req, res) {
   res.sendFile(__dirname + "/index.html");
 });
+app.get("/write", function (req, res) {
+  res.sendFile(__dirname + "/write.html");
+});
